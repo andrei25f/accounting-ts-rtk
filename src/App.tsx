9e8a2899b4a13accfeec2fc42f325b30eaae83e3
Andrey <andrey25f@gmail.com>
2024-09-24 09:57:47 +0200

    add routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,16 @@
+import { Navigate, Route, Routes } from 'react-router-dom'
 import './App.css'
 import Guest from './components/Guest'
 import Profile from './components/Profile'
 
 function App() {
+  const token = 'osiadoi';
+
   return (
-    <Profile/>
+    <Routes>
+      <Route path='/' element={token ? <Navigate to='/profile' /> : <Guest/>}/>
+      <Route path='/profile' element={token ? <Profile/> : <Navigate to='/' />}/>
+    </Routes>
   )
 }
 
