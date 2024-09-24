84272deb2889ae14b988cd155cdb8815d89fe8b6
Andrey <andrey25f@gmail.com>
2024-09-24 11:49:10 +0200

    add extraReducers

--- src/features/slices/tokenSlice.ts
+++ src/features/slices/tokenSlice.ts
@@ -1,4 +1,5 @@
 import { createSlice } from "@reduxjs/toolkit";
+import { changePassword, fetchUser, registerUser } from "../api/accountApi";
 
 const tokenSlice = createSlice({
     name: 'token',
@@ -6,6 +7,12 @@ const tokenSlice = createSlice({
     reducers: {
         putToken: (_state, action) => action.payload,
         deleteToken: () => ''
+    },
+    extraReducers: builder => {
+        builder
+            .addCase(registerUser.fulfilled, (_state, action) => action.payload.token)
+            .addCase(fetchUser.fulfilled, (_state, action) => action.payload.token)
+            .addCase(changePassword.fulfilled, (_state, action) => action.payload)
     }
 })
 
--- src/features/slices/userSlice.ts
+++ src/features/slices/userSlice.ts
@@ -1,12 +1,22 @@
 import { createSlice } from "@reduxjs/toolkit";
 import { UserProfile } from "../../utils/types";
+import { fetchUser, registerUser, updateUser } from "../api/accountApi";
 
 const userSlice = createSlice({
     name: 'user',
     initialState: {} as UserProfile,
     reducers: {
         putUser: (_state, action) => action.payload,
-        deleteUser: () => {}
+        deleteUser: () => { }
+    },
+    extraReducers: builder => {
+        builder
+            .addCase(registerUser.fulfilled, (_state, action) => action.payload.user)
+            .addCase(fetchUser.fulfilled, (_state, action) => action.payload.user)
+            .addCase(updateUser.fulfilled, (state, action) => {
+                state.firstName = action.payload.firstName;
+                state.lastName = action.payload.lastName;
+            })
     }
 })
 
